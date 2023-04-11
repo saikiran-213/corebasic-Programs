b45aedebe7882928da4d707500c2347744c1a84b
saikiran-213 <galalasaikiran03@gmal.com>
2023-04-11 22:04:20 +0530

    Added: Check to to Compute Quotient and Remainder

---
+++ src/CoreProgram/QuotientRemainderProgram.java
@@ -0,0 +1,17 @@
+package CoreProgram;
+
+public class QuotientRemainderProgram {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Check to Compute Quotient and Remainder.
+		 */
+        int num1 = 10, num2 = 2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+    }
+}
